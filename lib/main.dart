025d7b3406142e92615038e817e75a580f0f7016
faizza.mohammed <faizza.mohammed@gmail.com>
2022-01-12 23:05:44 +0530

    eg

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import './HomePage.dart';
+import './Timer_Data.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ChangeNotifierProvider(
+          create: (BuildContext context) => Timer_Data(), child: HomePage()),
+    );
+  }
+}
